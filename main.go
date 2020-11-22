81e6732fe151e67b317b49298011b2708502dc6d
Кирилл Викторович <four@MacBook-Pro-Kirill.local>
2020-11-22 16:34:58 +0300

    Simple example how work scraping in golang

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"github.com/PuerkitoBio/goquery"
+)
+
+func ExampleScrape() {
+	// Request the HTML page.
+
+	// create file 
+	file, err := os.Create("log.txt")
+
+	res, err := http.Get("https://stackoverflow.com/questions/tagged/javascript")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+	
+	if res.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+	}
+
+	// Load the HTML document
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Find the review items
+	doc.Find(".question-summary .summary").Each(func(i int, s *goquery.Selection) {
+		title := s.Find("H3").Text()
+		fmt.Println(i, title)
+		file.WriteString(title + "\n\n")
+
+	})
+}
+
+func main() {
+	ExampleScrape()
+}
